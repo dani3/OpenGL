a3bcc31c7c76083b4c5968683d4f93f7ff914541
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-07-21 22:03:12 +0200

    Elements used insted of vertex

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -120,23 +120,31 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float positions[6] = {
+	float positions[] = {
 		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f
+		 0.5f, -0.5f,
+		 0.5f,  0.5f,
+		-0.5f,  0.5f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
 	};
 
 	unsigned int buffer;
-	// Create a new buffer
 	glGenBuffers(1, &buffer);
-	// Select it using the identifier
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	// Populate the buffer 
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void *) 0);
 	
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
 	std::cout << "VERTEX" << std::endl;
@@ -151,7 +159,7 @@ int main(void)
 		// Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		// Swap front and back buffers
 		glfwSwapBuffers(window);
